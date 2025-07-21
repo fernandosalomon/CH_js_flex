91b0f7380d5b5e5e530f871432fab4b9445280bf
fernandosalomon <fernandosalomon1992@gmail.com>
2025-07-21 15:58:43 -0300

    first commit with basic structure for entregable_N1

---
+++ entregable_N1/src/options.js
@@ -0,0 +1,75 @@
+options = [
+    {
+        "name": "Pan",
+        "max_amout_to_choose": {
+            "big_combo": 1,
+            "normal_combo": 1,
+        },
+        "options": {
+            'Trigo blanco':120, 
+            'Semillas':130, 
+            'Focaccia':150, 
+            'Ciabatta':150,
+            'Salvado':130, 
+            'Sin Gluten': 140
+        }
+    },
+    {
+        "name": "Proteina",
+        "max_amount_to_choose": {
+            "big_combo": 2,
+            "normal_combo": 1,
+        },
+        "options": {
+            'Hummus': 1008, 
+            'Carne vacuna': 965, 
+            'Pollo': 297, 
+            'Atún': 792, 
+            'Pavo': 1240,
+        }
+    },
+    {
+        "name": "Quesos",
+        "max_amount_to_choose": {
+            "big_combo": 2,
+            "normal_combo": 1,
+        },
+        "options":{
+            'Cheddar': 210,
+            'Mozzarella': 210,
+            'Mar del Plata': 240, 
+            'Queso crema': 150,
+        },
+    },
+    {
+        "name": "Agregados",
+        "max_amount_to_choose":{
+            "big_combo": 3,
+            "normal_combo": 2,
+        },
+        "options":{
+            'Lechuga': 100, 
+            'Tomate': 100, 
+            'Pimientos': 200, 
+            'Avocado': 800, 
+            'Bacon': 1500,
+        },
+    },
+    {
+        "name": "Aderezos",
+        "max_amount_to_choose":{
+            "big_combo": 3,
+            "normal_combo": 2,
+        },
+        "options":{
+            'Mayonesa': 50,
+            'Savora': 50,
+            'Savora con miel': 70,
+            'Salsa picante': 70,
+            'Pesto': 80,
+        },
+    }
+]
+
+alert('Bienvenido a SandwitchMania\nDonde puedes armar tu sandwitch como tu quieras')
+alert('A continuación te daremos las opciones para que armes tu sandwitch y al final veras el precio a pagar en caja.\nCOMIENZA A ARMAR TU SANDWITCH AHORA')
