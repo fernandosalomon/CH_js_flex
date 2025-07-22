1e351516c4f663873892b5085f75ecb59e925d99
fernandosalomon <fernandosalomon1992@gmail.com>
2025-07-22 12:24:18 -0300

    add function to select combo

--- entregable_N1/src/options.js
+++ entregable_N1/src/options.js
@@ -71,5 +71,28 @@ options = [
     }
 ]
 
-alert('Bienvenido a SandwitchMania\nDonde puedes armar tu sandwitch como tu quieras')
-alert('A continuación te daremos las opciones para que armes tu sandwitch y al final veras el precio a pagar en caja.\nCOMIENZA A ARMAR TU SANDWITCH AHORA')
+// alert('Bienvenido a SandwitchMania\nDonde puedes armar tu sandwitch como tu quieras')
+// alert('A continuación te daremos las opciones para que armes tu sandwitch y al final veras el precio a pagar en caja.\nCOMIENZA A ARMAR TU SANDWITCH AHORA')
+
+function set_combo(){
+    const message = "Elije el tamaño de tu combo:\n\n1. Normal\n2. Grande\n0. Salir"
+    let chCombo = prompt(message)
+
+    switch(chCombo){
+        case '1':
+            return 'normal_combo';
+            break;
+        case '2':
+            return 'big_combo';
+            break;
+        case '0':
+            break;
+        default:
+            alert('La opción elejida no es válida. Pruebe de vuelta');
+            chCombo = set_combo();
+    }
+    
+    return chCombo;
+}
+
+let combo = set_combo();
