547456ff3f259117553d5047139f2c5145dd3559
fernandosalomon <fernandosalomon1992@gmail.com>
2025-08-12 18:41:02 -0300

    redo html structure to make everything more simple

--- entregable_N2/js/app.js
+++ entregable_N2/js/app.js
@@ -101,9 +101,8 @@ const prices = {
   },
 };
 
-options = [
-  {
-    name: "Pan",
+options = {
+  pan: {
     maxAmountToChoose: {
       big: 1,
       normal: 1,
@@ -118,8 +117,7 @@ options = [
     ],
   },
 
-  {
-    name: "Proteina",
+  proteina: {
     maxAmountToChoose: {
       big: 2,
       normal: 1,
@@ -127,8 +125,7 @@ options = [
     options: ["Hummus", "Carne_vacuna", "Pollo", "Atun", "Pavo"],
   },
 
-  {
-    name: "Quesos",
+  queso: {
     maxAmountToChoose: {
       big: 2,
       normal: 1,
@@ -136,8 +133,7 @@ options = [
     options: ["Cheddar", "Mozzarella", "Mar_del_plata", "Queso_crema"],
   },
 
-  {
-    name: "Agregados",
+  agregado: {
     maxAmountToChoose: {
       big: 3,
       normal: 2,
@@ -145,8 +141,7 @@ options = [
     options: ["Lechuga", "Tomate", "Pimientos", "Avocado", "Bacon"],
   },
 
-  {
-    name: "Aderezos",
+  aderezo: {
     maxAmountToChoose: {
       big: 3,
       normal: 2,
@@ -159,116 +154,97 @@ options = [
       "Pesto",
     ],
   },
-];
+};
 
 let userCH = [];
 let sandwitchSize = "";
 
-function setSandwitchSize(option){
-  sandwitchSize = option;
-  btn_normal = document.getElementById('idSandwitchSizeNormal')
-  btn_big = document.getElementById('idSandwitchSizeBig')
-
-  if(option == 'normal'){
-    btn_normal.classList.add("btnSandwitchSize_selected")
-    btn_big.classList.remove("btnSandwitchSize_selected")
-  }else{
-    btn_normal.classList.remove("btnSandwitchSize_selected")
-    btn_big.classList.add("btnSandwitchSize_selected")
-  }
-}
-
-function remainingChoosings(options, max){
-  let nChOptions = userCH.map(ing => options.filter(opt => opt == ing)).flat().length
-  return max - nChOptions
-}
-
-function addItem(ingredient){
+function addItem(ingredient) {
   cardElement = document.getElementById(ingredient.id);
   unitMeter = document.getElementById(`um-${ingredient.id}`);
 
   userCH.push(ingredient.id);
 
-  let ingredientAmount = userCH.filter(ing => ing == ingredient.id).length;
-    
+  let ingredientAmount = userCH.filter((ing) => ing == ingredient.id).length;
+
   unitMeter.classList.add("d-flex");
   unitMeter.innerHTML = `x${ingredientAmount}`;
   cardElement.classList.add("option-card_selected");
-
-  console.log(remainingChoosings([
-      "Mayonesa",
-      "Savora",
-      "Savora_con_miel",
-      "Salsa_picante",
-      "Pesto",
-    ], 3));
 }
 
-function removeItem(ingredient){
+function removeItem(ingredient) {
   cardElement = document.getElementById(ingredient.id);
   unitMeter = document.getElementById(`um-${ingredient.id}`);
 
-  index = userCH.findIndex(ing => ing == ingredient);
+  index = userCH.findIndex((ing) => ing == ingredient);
   userCH.splice(index, 1);
 
-  let ingredientAmount = userCH.filter(ing => ing == ingredient.id).length;
-    
-  unitMeter.innerHTML = `x${ingredientAmount}`;
-  
+  let ingredientAmount = userCH.filter((ing) => ing == ingredient.id).length;
 
-  if(ingredientAmount <= 0){
-    unitMeter.classList.remove('d-flex');
+  unitMeter.innerHTML = `x${ingredientAmount}`;
+
+  if (ingredientAmount <= 0) {
+    unitMeter.classList.remove("d-flex");
     cardElement.classList.remove("option-card_selected");
   }
 }
 
-const mainWrapper = document.getElementById("optionsWrapper");
+//Seleccionar el tamaño del sandwitch
 
-options.map((option) => {
+const sandwitchSizeBtnGrp = document.getElementById("sandwitchSizeBtnGrp");
 
-  const optionWrapper = document.createElement("div");
+function setSandwitchSize(option) {
+  sandwitchSize = option;
 
-  const comboSize = document.createElement("div");
-  const comboSizeTitle = document.createElement("h2");
-  comboSizeTitle.innerText = "Elige el tamaño de tu sandwitch";
-  comboSize.appendChild(comboSizeTitle);
-  const comboSizeOptions = document.createElement("div");
-  comboSizeOptions.classList.add("d-flex", "justify-content-center", "gap-5")
-  comboSizeOptions.id = 'comboSizeOptions';
-  comboSizeOptions.innerHTML = `
-    <div classname='card option-card' onClick='(() => setSandwitchSize("normal"))()' id='idSandwitchSizeNormal'>
-      <img src='../img/normal.png' class="card-img-top" alt="Tamaño normal" style="max-height: 140px;">
-      <div class="card-body">
-        <h5 class="text-center">Normal</h5>
-      </div>
-    </div>
-    <div classname='card option-card' onClick='(() => setSandwitchSize("big"))()' id='idSandwitchSizeBig'>
-      <img src='../img/big.png' class="card-img-top" alt="Tamaño normal" style="max-height: 140px;">
-      <div class="card-body">
-        <h5 class="text-center">Grande</h5>
-      </div>
-    </div>
-  `
-  comboSize.appendChild(comboSizeOptions);
-  optionWrapper.appendChild(comboSize);
+  for (
+    let i = 0;
+    i < sandwitchSizeBtnGrp.querySelectorAll(".btn").length;
+    i++
+  ) {
+    sandwitchSizeBtnGrp.querySelectorAll(".btn")[i].classList =
+      "btn sandwitchSizeBtn";
+  }
 
+  sandwitchSizeBtnGrp
+    .querySelectorAll(".btn")
+    [sandwitchSize].classList.add("sandwitchSizeBtn_active");
+}
+
+for (let i = 0; i < sandwitchSizeBtnGrp.querySelectorAll(".btn").length; i++) {
+  sandwitchSizeBtnGrp
+    .querySelectorAll(".btn")
+    [i].addEventListener("click", () => setSandwitchSize(i));
+}
+
+//Agregar las cards a cada seccion
+
+for (const [key, value] of Object.entries(options)) {
+  const wrapper = document.getElementById(key);
 
   //Titulo de la opción
   const title = document.createElement("h2");
-  title.classList.add("mb-4")
+  title.classList.add("mb-4");
   title.innerText = `
-    Elegí tu/s opcion/es de ${option.name} (Te quedan ${option.maxAmountToChoose["big"]})
+    Elegí tu/s opcion/es de ${key} (Te quedan ${value.maxAmountToChoose["big"]})
   `;
-  optionWrapper.appendChild(title);
-  
+  wrapper.appendChild(title);
+
   //Sistema Grid para las opciones
   const row = document.createElement("div");
-  row.classList.add("row")
-  
-  option.options.map((ing) => {
- 
+  row.classList.add("row");
+
+  value.options.map((ing) => {
     const cardWrapper = document.createElement("div");
-    cardWrapper.classList.add("col-12", "col-sm-6", "col-md-4", "col-lg-3", "gy-3", "gx-3", "card", "option-card");
+    cardWrapper.classList.add(
+      "col-12",
+      "col-sm-6",
+      "col-md-4",
+      "col-lg-3",
+      "gy-3",
+      "gx-3",
+      "card",
+      "option-card"
+    );
     cardWrapper.id = ing;
     cardWrapper.innerHTML = `
       <img src=${prices[ing].image} class="card-img-top" alt="${ing}" style="min-height: 7rem;">
@@ -284,17 +260,13 @@ options.map((option) => {
 
     const unitMeter = document.createElement("div");
     unitMeter.id = `um-${ing}`;
-    unitMeter.innerHTML = 'x2';
+    unitMeter.innerHTML = "x2";
     unitMeter.classList.add("unitMeter");
 
     cardWrapper.appendChild(unitMeter);
 
     row.appendChild(cardWrapper);
-    
-
   });
 
-  optionWrapper.appendChild(row);
-  mainWrapper.appendChild(optionWrapper);
-
-});
+  wrapper.appendChild(row);
+}
