f1e4bd12cf8b22ca9ed5a1e3b788398e0ca43da9
fernandosalomon <fernandosalomon1992@gmail.com>
2025-09-01 19:04:13 -0300

    add options json and modal basic structure

--- entrega_final/js/app.js
+++ entrega_final/js/app.js
@@ -1,13 +1,3 @@
-async function loadJSON(path) {
-  try {
-    const res = await fetch(path);
-    if (!res.ok) {
-      throw new Error("No se pudo obtener los datos.");
-    }
-    return res.json();
-  } catch (error) {
-    console.error("Error: ", error);
-  }
-}
+
 
 
