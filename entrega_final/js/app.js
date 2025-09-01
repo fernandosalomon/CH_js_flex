e83c2c7978d5f51ba22e4bd3805dd43a8c87ba88
fernandosalomon <fernandosalomon1992@gmail.com>
2025-09-01 19:31:39 -0300

    add sandwitch size selection btn

--- entrega_final/js/app.js
+++ entrega_final/js/app.js
@@ -1,3 +1,16 @@
+const sizeNormal = document.querySelector("#sizeNormal");
+const sizeBig = document.querySelector("#sizeBig");
+let size = 0;
 
+sizeNormal.addEventListener("click", () => {
+  size = 0;
+  sizeNormal.classList.add("size-btn_selected");
+  sizeBig.classList.remove("size-btn_selected");
+});
 
+sizeBig.addEventListener("click", () => {
+  size = 1;
+  sizeNormal.classList.remove("size-btn_selected");
+  sizeBig.classList.add("size-btn_selected");
+});
 
